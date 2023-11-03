15b5ca5e8c119782a0960a213e7df53004af7549
Mutapha Pelumi Babatunde <97739416+oxblixxx@users.noreply.github.com>
2023-11-03 01:36:34 +0100

    Update main.tf

--- compute_engine/main.tf
+++ compute_engine/main.tf
@@ -20,31 +20,6 @@ terraform {
   }
 }
 
-module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 6.0"
-
-    project_id   = "qwiklabs-gcp-03-63e3ae316a5a"
-    network_name = "tf-vpc-120295"
-    routing_mode = "GLOBAL"
-
-    subnets = [
-        {
-            subnet_name           = "subnet-01"
-            subnet_ip             = "10.10.10.0/24"
-            subnet_region         = "us-east1"
-        },
-        {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = "us-east1"
-          #  subnet_private_access = "true"
-          #  subnet_flow_logs      = "true"
-          #  description           = "This subnet has a description"
-        }
-     ]
-}
-
 resource "google_compute_firewall" "tf-firewall" {
   name    = "tf-firewall"
   network = "projects/qwiklabs-gcp-03-63e3ae316a5a/global/networks/tf-vpc-120295"
